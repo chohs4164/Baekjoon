ed8110677d20db1d59c0e0aa23273814ba4d95c2
chohs4164 <138971722+chohs4164@users.noreply.github.com>
2024-01-19 16:25:16 +0900

    Add files via upload

---
+++ 2738.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int N, M; //N:ї­,M:За
+	int A[100][100],B[100][100];
+
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &A[i][j]);
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &B[i][j]);
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%d ",A[i][j]+B[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
